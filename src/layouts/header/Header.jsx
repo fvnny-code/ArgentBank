9aef7c4f9816e0f1e67f72a0d9ed2dd5040d5ff2
fvnny-code <f.codecco.grando@gmail.com>
2023-07-10 15:01:23 +0200

    implementation of conditional display of Header elements

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -3,12 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOut, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 import logo from "../../assets/img/argentBankLogo.png";
-
 import "./Header.css";
 
+import { useSelector } from "react-redux";
+import {clearCredentials} from "../../store/Autentication/token"
+
+
 export default function Header() {
+  
+  const isAuth  = useSelector((store) => store.token);
+  // const userName = useSelector((store)=> store.user)
+  
   return (
-    <>
+    <> 
       <nav className="main-nav">
         <NavLink to="/" className="main-nav-logo">
           <img
@@ -19,9 +26,22 @@ export default function Header() {
           <h1 className="sr-only">Argent Bank</h1>
         </NavLink>
         <div>
-          {/* Anonymous */}
-        
-            <>
+          <>
+            {isAuth ? (
+              <>
+                <NavLink to="/profile" className="main-nav-item">
+                  <FontAwesomeIcon
+                    icon={faUserCircle}
+                    className="main-nav-icon"
+                  />
+                  {/* {userName.firstName} */}
+                </NavLink>
+                <NavLink to="/" className="main-nav-item" onClick={clearCredentials()} >
+                  <FontAwesomeIcon icon={faSignOut} className="main-nav-icon" />
+                  Sign Out
+                </NavLink>
+              </>
+            ) : (
               <NavLink to="/login" className="main-nav-item">
                 <FontAwesomeIcon
                   icon={faUserCircle}
@@ -29,27 +49,8 @@ export default function Header() {
                 />
                 Sign In
               </NavLink>
-            </>
-          
-          {/* Connected */}
-         
-            <>
-              <NavLink to="/profile" className="main-nav-item">
-                <FontAwesomeIcon
-                  icon={faUserCircle}
-                  className="main-nav-icon"
-                />
-                Username
-              </NavLink>
-              <NavLink to="/" className="main-nav-item">
-                <FontAwesomeIcon
-                icon={faSignOut}
-                className="main-nav-icon"
-                />
-                Sign Out
-              </NavLink>
-            </>
-          
+            )}
+          </>
         </div>
       </nav>
     </>
